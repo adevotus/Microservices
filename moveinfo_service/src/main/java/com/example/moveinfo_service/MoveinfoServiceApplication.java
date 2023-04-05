0a9892f90bfb30771eb3740af7c3ebadd483ec9a
Adevotus Dickson <73017158+adevotus@users.noreply.github.com>
2023-04-05 12:43:07 +0300

    microservices

---
+++ moveinfo_service/src/main/java/com/example/moveinfo_service/MoveinfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.moveinfo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveinfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveinfoServiceApplication.class, args);
+	}
+
+}
---
+++ moveinfo_service/src/test/java/com/example/moveinfo_service/MoveinfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moveinfo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveinfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
